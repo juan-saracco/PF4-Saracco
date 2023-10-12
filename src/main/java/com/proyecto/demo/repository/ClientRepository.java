a4e14f0c5b3390ab95b842d875f8d610b8caf09e
juan-saracco <juansaracco1212@gmail.com>
2023-10-12 11:51:39 -0300

    [ADD] CLIENT done

--- src/main/java/com/proyecto/demo/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.demo.repository;
-
-import com.proyecto.demo.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
--- src/main/java/com/proyecto/demo/repository/InvoiceDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.demo.repository;
-
-import com.proyecto.demo.entity.InvoiceDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceDetailsRepository extends JpaRepository<InvoiceDetails, Integer> {
-}
